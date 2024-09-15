5f2fd763ecac2d8fbab2d661dc247a8e9a4f1a6a
abubakarasif7207 <abubakarprofessional7207@gmail.com>
2024-09-15 14:46:58 +0500

    Add files via upload

---
+++ color.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main(){
+          system("color 02");
+cout<<"color change"<<endl;                                                  
+  cout<<"         ### ##        ##          ## ##       ##   ##     ##         ###  ##    "   <<  endl;                                 
+  cout<<"          ##   ##       ##        ##   ##       ## ##       ##          ## ##     "  <<  endl;                           
+  cout<<"          ##   ##    ##  ##       ##           # ### #    ## ##        # ## #      " <<  endl;                    
+  cout<<"          ##   ##    ##  ##       ##           ## # ##    ##  ##       ## ##       " <<  endl; 
+  cout<<"          ##  ##     ## ####      ##           ##   ##    ## ###       ##  ##      " <<  endl; 
+  cout<<"          ##         ##   ##      ##   ##      ##   ##    ##  ##       ##  ##      " <<  endl; 
+  cout<<"         ####        ##   ##       ## ##       ##   ##   ###  ##      ###  ##      " <<  endl; 
+    }
+
+
+
