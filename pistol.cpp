4c0b72fc27a941f072d8960ca4bd49287aa94464
abubakarasif7207 <abubakarprofessional7207@gmail.com>
2024-09-15 14:48:16 +0500

    Add files via upload

---
+++ pistol.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main(){
+
+
+cout<<"                             +--^---------- -------- ----- --------^-      "<<endl; 
+cout<<"                                           '        '     '          '     "<<endl; 
+cout<<"                              | |||||||||   '--------'     |          o    "<<endl; 
+cout<<"                              '+---------------------------^----------|    "<<endl;           
+cout<<"                                ' ---------- --------- --------------'     "<<endl; 
+cout<<"                                 '          '         '                    "<<endl; 
+cout<<"                                  / XXXXXX /' |        /'                  "<<endl; 
+cout<<"                                 / XXXXXX /   |       /'                   "<<endl; 
+cout<<"                                /XXXXXXX /'--------'                       "<<endl; 
+cout<<"                               /XXXXXXX /                                  "<<endl; 
+cout<<"                              /XXXXXXX /                                   "<<endl; 
+cout<<"                             (--------(                                    "<<endl; 
+cout<<"                              '------'                                     "<<endl; 
+}
